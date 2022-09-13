c27eb25eecf79fc3e0dff910017c8dab70b0edd7
Fran Desktop <franchi13579@gmail.com>
2022-09-13 09:26:04 -0300

    Homework Clase 01

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,25 +1,23 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  let aux = 1, sum = 0, num2 = num.toString(), aux2 = 0;
-  for (let i=(num2.length - 1); i >= 0; i--){
-    aux2 = parseInt(num2[i]);
-    sum = sum + (aux2 * aux);
-    aux = aux * 2;
-  }
+  let arr = [], sum = 0, exp = 0;
+  arr = num.split("");
+  arr.reverse();
+  arr.forEach(e => {
+    sum = sum + arr[e]*(2**exp)
+    exp++;
+  });
   return sum;
 }
 
 function DecimalABinario(num) {
-  let aux = 0, aux2 = "", numf = "";
-  for (let i=num; i > 0; i){
-  aux = (i%2);
-  i = (i/2)
-  i = Math.floor(i)
-  aux2 = aux.toString();
-  numf = aux2 + numf;
+  let arr = [];
+  for (let i = 0; num > 0; i++){
+    arr.push(num%2);
+    num = Math.floor(num/2);
   }
-  return numf;
+  return arr.reverse().join("");
 }
 
 
