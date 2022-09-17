da00cac3da273d25e55fa14dc8466542b56f02de
Fran Desktop <franchi13579@gmail.com>
2022-09-17 19:06:31 -0300

    Homework Clase 05 cr

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -25,41 +25,34 @@ LinkedList.prototype.add = function (data) {
   let node = new Node(data);
 
   let current = this.head;
-  if (this.head === null) {
+  if (!this.head) { // this.head === null
     this.head = node; 
-    return;
-  }
+    return; // corta la ejecucion 
+  };
 
-  while (current.next !== null){
+  while (current.next){ // current.next !== null
     current = current.next;
-  }
+  };
   current.next = node;
-  
-  // bucle infinito en el primer node? 
-  // if(current !== this.head){
-  //   current.next = node;
-  // }
-}
+};
   
 
 LinkedList.prototype.remove = function () {
   let current = this.head;
-  if (current === null) {
-    return null;
-  };
+  if (!current) return null; // current === null 
 
-  if (current.next === null){
+  if (!current.next){ // current.next === null
     this.head = null;
     return current.value;
   };
 
-  while (current.next.next !== null){
+  while (current.next.next){ // current.next.next !== null
     current = current.next;
   };
   let aux = current.next;
   current.next = null;
   return aux.value;
-}
+};
 
 // - search: recibe un parámetro y lo busca dentro de la lista, con una particularidad: el parámetro puede ser un valor o un callback. 
 //   En el primer caso, buscamos un nodo cuyo valor coincida con lo buscado; en el segundo, buscamos un nodo cuyo valor, al ser pasado como parámetro del callback, retorne true. 
@@ -70,30 +63,27 @@ LinkedList.prototype.remove = function () {
 
 LinkedList.prototype.search = function (x) {
   let current = this.head;
-  if (!current) {
-    return null;
-  }
+  if (!current) return null; // current === null 
   if (typeof(x) !== 'function'){
-    while (current.value !== x && current.next !== null){
+    while (current.value !== x && current.next){ // current.next !== null
       current = current.next;
-    }
+    };
     if (current.value === x){
       return current.value;
     } else {
-      return null
-    }
+      return null;
+    };
   } else {
-    while (x(current.value) !== true && current.next !== null){
+    while (!x(current.value) && current.next){ // x(current.value) !== true && current.next !== null 
       current = current.next;
-    }
-    if (x(current.value) === true){
+    };
+    if (x(current.value)){ // x(current.value) === true
       return current.value;
     } else {
       return false;
-    }
-  }
-  
-}
+    };
+  };
+};
 
 /*
 Implementar la clase HashTable.
@@ -115,25 +105,21 @@ Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero
  luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
 function HashTable() {
-  let table = [];
-  table.length = 35;
-	this.numBuckets = table.length;
+	this.numBuckets = 35;
+  this.buckets = [];
 };
 
 HashTable.prototype.hash = function (key) {
   let pos = 0;
   if(typeof(key) !== "string") throw new TypeError("no es un string");
-  for (let i=0;i<key.length;i++) pos = pos + key.charCodeAt(i);
-  return pos%35;
+  for (let i=0;i<key.length;i++) pos += key.charCodeAt(i);
+  return pos%this.numBuckets;
 };
 
 HashTable.prototype.set = function(key, value) {
   let pos = this.hash(key);
-  if (this[pos] === undefined || this[pos][key] !== undefined){
-    this[pos] = {[key] : value};
-  } else {
-    Object.defineProperty(this[pos], key, {value: value});
-  };
+  if (this[pos] === undefined) this[pos] = {};
+  this[pos][key] = value;
 };
 
 HashTable.prototype.get = function(key) {
@@ -143,12 +129,28 @@ HashTable.prototype.get = function(key) {
 
 HashTable.prototype.hasKey = function(key) {
   let pos = this.hash(key);
-  if (this[pos][key] !== undefined){
-    return true;
-  } else {
-    return false;
-  };
+  return this[pos].hasOwnProperty(key);
 };
+
+// Antes del code-rewiew
+
+// HashTable.prototype.set = function(key, value) {
+//   let pos = this.hash(key);
+//   if (this[pos] === undefined || this[pos][key] !== undefined){
+//     this[pos] = {[key] : value};
+//   } else {
+//     Object.defineProperty(this[pos], key, {value: value});
+//   };
+// };
+
+// HashTable.prototype.hasKey = function(key) {
+//   let pos = this.hash(key);
+//   if (this[pos][key] !== undefined){
+//     return true;
+//   } else {
+//     return false;
+//   };
+// };
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
