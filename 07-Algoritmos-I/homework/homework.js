85f9bb5c60945a93209891be42f95bc2a6f5627e
Fran Desktop <franchi13579@gmail.com>
2022-09-20 14:57:04 -0300

    Homework Clase 07

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,34 +6,81 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
-}
+  let array = [1];
+  for (let i=2; i<=num ; i++){
+    if (num % i === 0){
+      num = num / i;
+      array.push(i);
+      i--;
+    };
+  };
+  return array;
+};
 
 function bubbleSort(array) {
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
-}
-
+  for (let i=0; i<array.length; i++){
+    for (let j=0; j<array.length; j++){
+      if(array[j] > array[j+1]){
+        let aux = array[j+1];
+        array[j+1] = array[j];
+        array[j] = aux; 
+      } ;
+    };
+  };
+  return array;
+};
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for(let i=1; i<array.length; i++){
+    let j = i-1
+    let aux = array[i]
+    while(j>=0 && aux < array[j]){
+      array[j+1] = array[j];
+      j--;
+    }
+    array[j+1] = aux;
+  }
+  return array;
 
-}
-
+  // for (let i=1; i<array.length; i++){
+  //   for (let j=(i-1); j>=0; j--){
+  //     if(array[i] < array[j]){
+  //       let aux = array[i];
+  //       array[i] = array[j];
+  //       array[j] = aux;
+  //       i--;
+  //     };
+  //   };
+  // };
+  // return array;
+};
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
-}
+  for (let i=0; i<array.length; i++){ 
+    let min = i;
+    for (let j=(i+1); j<array.length; j++){
+      if(array[j] < array[min]){
+        min = j;
+      };
+    };
+    let aux = array[i];
+    array[i] = array[min];
+    array[min] = aux; 
+  };
+  return array;
+};
 
 
 // No modificar nada debajo de esta línea
